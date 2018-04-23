ba76c455da67bf905666bbe52264eef87f336b88
Adriana Castaneda <adriana.lcs316@gmail.com>
2018-04-23 11:14:05 -0500

    took logic out of view

--- app.rb
+++ app.rb
@@ -16,11 +16,29 @@ def sanitize_form(form_params)
 	return sanitized_params
 end
 
+def format_results(restaurants)
+	results= []
+	restaurants.each do |restaurant|
+		restaurant["inspection_date"]=restaurant["inspection_date"][0..-14]
+		if restaurant.key?("violations")
+			violations_array = restaurant["violations"].gsub("\n",'').split('|')
+			restaurant["violations"]=violations_array.map do |violation|
+				violation.downcase.gsub(/[a-z][^.?!]*/){|m| m.capitalize}
+			end
+		end
+		results<<restaurant
+	end
+	p results
+	return results
+end
+
 def find_restaurants(sanitized_params)
-	results = HTTParty.get('https://data.cityofchicago.org/resource/cwig-ma7x.json', 
+	request = HTTParty.get('https://data.cityofchicago.org/resource/cwig-ma7x.json', 
 		{headers: {"X-App-Token": "bjp8KrRvAPtuf809u1UXnI0Z8"},
 		query: sanitized_params})
-	if results.code == 200 && results.parsed_response != []
+	if request.code == 200 && request.parsed_response != []
+		p request
+		results = format_results(request)
 		return results
 	else
 		return "Something went wrong. Check your inputs."
